c658b2d8eb80a5f77e85c86f9fb7d35326180163
samdev2200 <samuel2200@hotmail.com>
2025-05-22 17:04:19 +0200

    Clase UsuarioEntity para el login (hecho)

--- src/main/java/es/javawarweb/web/entities/UsuarioEntity.java
+++ src/main/java/es/javawarweb/web/entities/UsuarioEntity.java
@@ -1,12 +1,74 @@
 package es.javawarweb.web.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "TB_USUARIOS")
+@Table(name = "TB_USUARIOS_WAR")
 public class UsuarioEntity {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	
+	@Column(name = "NOMBRE", nullable = false)
+	private String nombre;
+	
+	@Column(name = "EMAIL", nullable = false, unique = true)
+	private String email;
+	
+	@Column(name = "CONTRASEÑA", nullable = false)
+	private String contrasenia;
+
+	// Constructores
+	public UsuarioEntity() {}
+	
+	public UsuarioEntity(String email, String contrasenia) {
+		super();
+		this.email = email;
+		this.contrasenia = contrasenia;
+	}
+	
+	public UsuarioEntity(String email, String contrasenia, String nombre) {
+		this(email, contrasenia);
+		this.nombre = nombre;
+	}
+	
+	// Getter and setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getContrasenia() {
+		return contrasenia;
+	}
+
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
 	
 }
